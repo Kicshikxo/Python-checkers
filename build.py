17303ded059469ec11d1c8eba6c28869f6d95c56
Kicshikxo <supermegavalera9000@gmail.com>
2021-11-03 02:07:34 +0300

    updated build script

--- build.py
+++ build.py
@@ -4,6 +4,6 @@ import platform
 system = platform.system()
 
 if (system == 'Windows'):
-    os.system("pyinstaller -F -w -i icon.ico main.py --hidden-import='PIL._tkinter_finder'")
+    os.system("pyinstaller -F -w -i icon.ico main.py --hidden-import='PIL._tkinter_finder' && xcopy /Y assets/ dist/assets/ && xcopy /Y icon.png dist/")
 elif (system == 'Linux'):
-    os.system("pyinstaller -F -w -i icon.png main.py --hidden-import='PIL._tkinter_finder'")
+    os.system("pyinstaller -F -w -i icon.png main.py --hidden-import='PIL._tkinter_finder' && /bin/cp -rf assets/ dist/ && /bin/cp -f icon.png dist/")
