ec0725a3b7a2a8f41b53b6762989a0fb990f6ae4
Kicshikxo <supermegavalera9000@gmail.com>
2021-11-05 03:51:53 +0300

    added SideType.opposite

--- checkers/enums.py
+++ checkers/enums.py
@@ -4,6 +4,13 @@ class SideType(Enum):
     WHITE = auto()
     BLACK = auto()
 
+    def opposite(side):
+        if (side == SideType.WHITE):
+            return SideType.BLACK
+        elif (side == SideType.BLACK):
+            return SideType.WHITE
+        raise ValueError()
+
 class CheckerType(Enum):
     NONE = auto()
     WHITE_REGULAR = auto()
